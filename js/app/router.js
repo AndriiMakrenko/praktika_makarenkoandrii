e091689139e1aef11f14cbf6c877da2e1498a69d
Andrii <makarenkoandrii22@gmail.com>
2025-02-10 18:02:57 +0100

    Refacrot

--- js/app/router.js
+++ js/app/router.js
@@ -1,6 +1,8 @@
 import { login } from './pages/login.js';
 import { campaigns } from './pages/campaigns.js';
 import { campaign } from './pages/campaign.js';
+import { users } from './pages/users.js';
+import { user } from './pages/user.js';
 
 export const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
@@ -19,6 +21,16 @@ export const router = VueRouter.createRouter({
       path: '/campaign/:id',
       name: 'Campaign',
       component: campaign 
+    },
+    {
+      path: '/users',
+      name: 'Users',
+      component: users
+    },
+    {
+      path: '/user/:id',
+      name: 'User',
+      component: user
     }
   ]
 });
